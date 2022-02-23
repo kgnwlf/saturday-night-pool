3ac820d6293e4cbabf150bc8b1f7ef665fd00a30
Keegan Wolf <kwolf313@live.com>
2022-02-23 16:59:42 -0600

    Connected to Atlas DB, created player and game schemas, able to connect and get models

--- data/mongoose.js
+++ data/mongoose.js
@@ -1,10 +1,44 @@
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
+
+const players = new mongoose.Schema({
+  name: String,
+  win: Number,
+  loses: Number,
+  ratio: Number
+});
+
+const games = new mongoose.Schema({
+  winner: String,
+  loser: String,
+  timestamp: {type: Date, default: Date.now}
+});
+
+const player = mongoose.model('player', players);
+const game = mongoose.model('game', games);
+
 
 async function main() {
-  await mongoose.connect('mongodb+srv://saturdaypool:Keegansmells123@cluster0.u9jaj.mongodb.net/Cluster0?retryWrites=true&w=majority');
 
+  const conn = await mongoose.connect('mongodb://saturdaypool:Keegansmells123@cluster0-shard-00-00.u9jaj.mongodb.net:27017,cluster0-shard-00-01.u9jaj.mongodb.net:27017,cluster0-shard-00-02.u9jaj.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-nhjpk9-shard-0&authSource=admin&retryWrites=true&w=majority');
+
+ /*
+ get all players from atlas
+ get all games from atlas
+ */
 }
 
-module.exports.db = main();
+// update scores function
+
+// create game function
+
+main().catch(err => console.log(err));
+
+module.exports.db = main;
+
+// const test1 = new player ({name: 'test', win: 0, loses: 0, ratio: 0});
+// await test1.save();
+// console.log(test1.name);
+// const gtest1 = new game ({winner: 'test', loser: 'test2'});
+// await gtest1.save();
+// console.log(gtest1.name);
--- server.js
+++ server.js
@@ -5,11 +5,19 @@ var db = require('./data/mongoose');
 
 app.use(express.static('dist'));
 
-// app.get('/', function (req, res) {
-//   console.log('*** REQUEST: ', req);
-//   res.send('Server is working');
-// });
+
+// get request on succ load
+// call main
+
+// on name click to finish game
+// call update players
+// call create game
 
 app.listen(3000, function () {
   console.log('Listening on 127.0.0.1:3000...');
 });
+
+// app.get('/', function (req, res) {
+//   console.log('*** REQUEST: ', req);
+//   res.send('Server is working');
+// });
